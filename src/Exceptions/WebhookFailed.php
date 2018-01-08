28f7ccbce728659bee6cc3e387f4013f9658af59
Brent Roose <brent@spatie.be>
2018-01-08 15:15:16 +0100

    Fix typo (#26)

--- src/Exceptions/WebhookFailed.php
+++ src/Exceptions/WebhookFailed.php
@@ -14,7 +14,7 @@ class WebhookFailed extends Exception
 
     public static function invalidSignature($signature)
     {
-        return new static("The signature `{$signature}` found in the header named `Stripe-Signature` is invalid. Make sure that the `services.stripe.webhook_signing_secret` config key is set to the value you found on the Stripe dashboard. If you are caching your config try running `php artisan clear:cache` to resolve the problem.");
+        return new static("The signature `{$signature}` found in the header named `Stripe-Signature` is invalid. Make sure that the `services.stripe.webhook_signing_secret` config key is set to the value you found on the Stripe dashboard. If you are caching your config try running `php artisan cache:clear` to resolve the problem.");
     }
 
     public static function signingSecretNotSet()
