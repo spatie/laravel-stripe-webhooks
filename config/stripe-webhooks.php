0df3a72a8ad7862fa72e01416bce6135f68da073
Freek Van der Herten <freek@spatie.be>
2021-01-12 09:25:00 +0100

    Update stripe-webhooks.php

--- config/stripe-webhooks.php
+++ config/stripe-webhooks.php
@@ -1,8 +1,6 @@
 <?php
 
 return [
-    'verify_signature' => env('STRIPE_SIGNATURE_VERIFY', true),
-
     /*
      * Stripe will sign each webhook using a secret. You can find the used secret at the
      * webhook configuration settings: https://dashboard.stripe.com/account/webhooks.
@@ -26,4 +24,10 @@ return [
      * Spatie\StripeWebhooks\ProcessStripeWebhookJob.
      */
     'model' => \Spatie\StripeWebhooks\ProcessStripeWebhookJob::class,
+    
+    /*
+     * When disabled, the package will not verify if the signature is valid.
+     * This can be handy in local environments.
+     */
+    'verify_signature' => env('STRIPE_SIGNATURE_VERIFY', true),
 ];
